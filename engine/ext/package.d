3c8642e32d2dfb8618633702c056016b9dfced25
mkoskim <mkoskim@gmail.com>
2015-05-26 10:31:13 +0300

    Preparations for changes.

--- engine/ext/package.d
+++ engine/ext/package.d
@@ -9,11 +9,11 @@ module engine.ext;
 //-----------------------------------------------------------------------------
 
 public {
-    import geom = engine.ext.geom;
     import engine.ext.bitmap: Bitmap;
     import engine.ext.canvas;
     import engine.ext.font;
 
+    import geom = engine.ext.geom;
     import simple = engine.ext.simple;
 }
 
---
+++ engine/ext/simple/package.d
@@ -0,0 +1,12 @@
+//*****************************************************************************
+//
+// Some simplers to speed up certain things
+//
+//*****************************************************************************
+
+module engine.ext.simple;
+
+public {
+    import engine.ext.simple.gameloop: gameloop;
+}
+
