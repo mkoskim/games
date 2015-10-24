1e3f26e8005916aa9aec952e93017ebe5200dc47
Markus Koskimies <mkoskim@gmail.com>
2015-10-24 16:37:40 +0300

    Implemented Translate class.
    This is preparations for animation framework. Translation can give us
    stable transitions in specific range, making them useful to move
    things around.

--- engine/util.d
+++ engine/util.d
@@ -15,11 +15,9 @@ public import std.stdio: writeln, writefln, write, writef;
 public import std.string: format;
 public import std.conv: to;
 
-public import gl3n.linalg;
-public import std.math: abs;
-
 public import std.exception: enforce;
 public import engine.game.util: quit, quitif, ERROR, ERRORIF;
+public import engine.math;
 
 debug public import engine.game.track: Track;
 
@@ -36,35 +34,6 @@ void TODO(string msg = null,
         writefln("TODO: %s (%s)", msg, func);
     } else {
         writefln("TODO: %s", func);
-    }
-}
-
-//-----------------------------------------------------------------------------
-//
-// Manhattan distance is pretty quick to calculate, but probably nowadays
-// squared distance (x*x + y*y + z*z) is as fast.
-//
-//-----------------------------------------------------------------------------
-
-float manhattan(vec3 a, vec3 b)
-{
-    return abs(b.x-a.x) + abs(b.y-a.y) + abs(b.z-a.z);
-}
-
-//-----------------------------------------------------------------------------
-//
-// Sliding average is useful for various performance meters.
-//
-//-----------------------------------------------------------------------------
-
-class SlidingAverage
-{
-    float average = 0;
-
-    void update(float value)
-    {
-        const float window = 10.0;
-        average += (value - average) / window;
     }
 }
 
