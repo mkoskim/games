007e52cec0aa62475b21e7760a888433bda8327c
Markus Koskimies <mkoskim@gmail.com>
2017-04-22 13:20:42 +0300

    Removed test objects
    Game stock/ skething now happens locally, no need to push test models to
    this repository.

--- tools/objectview/objectview.d
+++ tools/objectview/objectview.d
@@ -24,6 +24,56 @@ void main()
     game.init(800, 600);
 
     //-------------------------------------------------------------------------
+    // Load asset
+    //-------------------------------------------------------------------------
+
+    auto scene =
+        //engine.asset.SceneGraph.load("engine/stock/unsorted/mesh/Suzanne/Suzanne.obj")
+        //engine.asset.SceneGraph.load("engine/stock/unsorted/mesh/Cube/Cube.dae")
+        //engine.asset.SceneGraph.load("engine/stock/unsorted/mesh/Chess/king.obj")
+        //engine.asset.SceneGraph.load("data/Girl/Girl.dae")
+        //engine.asset.SceneGraph.load("engine/stock/unsorted/tests/furnace.dae")
+        //engine.asset.SceneGraph.load("engine/stock/universe/cardboard/Female1.obj")
+        engine.asset.SceneGraph.load("local/stockset/sketches/Female.dae")
+        //engine.asset.SceneGraph.load("local/stockset/sketches/Female.obj")
+        ;
+
+    auto mesh = scene.meshes[0];
+
+    //-------------------------------------------------------------------------
+    // Textures are uploaded by loaders: these may have different sampling
+    // parameters.
+    //-------------------------------------------------------------------------
+
+    auto cm_loader = engine.gpu.Texture.Loader.Compressed;
+    auto nm_loader = engine.gpu.Texture.Loader.Default;
+
+    auto colormap =
+        cm_loader(vec4(0.5, 0.5, 0.5, 1))
+        //cm_loader("engine/stock/unsorted/tiles/AlienCarving/ColorMap.png")
+        //cm_loader("engine/stock/unsorted/tiles/BrickWall1/ColorMap.png")
+        //cm_loader("data/Girl/Girl_cm.png")
+        ;
+    colormap.info();
+
+    auto normalmap =
+        nm_loader(vec4(0.5, 0.5, 1, 0))
+        //nm_loader("engine/stock/unsorted/tiles/AlienCarving/NormalMap.png")
+        //nm_loader("engine/stock/unsorted/tiles/Concrete/Crusty/NormalMap.png")
+    ;
+
+    //-------------------------------------------------------------------------
+
+    auto mProjection = mat4.perspective(
+        game.screen.width, game.screen.height,
+        60,
+        1, 200
+    );
+
+    auto mView = mat4.look_at(vec3(0, -20, 3), vec3(0, 0, 2), vec3(0, 0, 1));
+    auto pLight = vec3(5, 3, 7);
+    
+    //-------------------------------------------------------------------------
     // Create shader from GLSL, and GPU/OpenGL "state" for the shader: it is
     // intended that these two are tightly coupled - state settings are
     // treated like shader parameters.
@@ -61,43 +111,6 @@ void main()
     auto family = state.shader.family;
 
     //-------------------------------------------------------------------------
-    // Load asset
-    //-------------------------------------------------------------------------
-
-    auto scene =
-        //engine.asset.SceneGraph.load("engine/stock/unsorted/mesh/Suzanne/Suzanne.obj")
-        //engine.asset.SceneGraph.load("engine/stock/unsorted/mesh/Cube/Cube.dae")
-        //engine.asset.SceneGraph.load("engine/stock/unsorted/mesh/Chess/king.obj")
-        //engine.asset.SceneGraph.load("data/Girl/Girl.dae")
-        engine.asset.SceneGraph.load("local/Female1.dae")
-        //engine.asset.SceneGraph.load("engine/stock/unsorted/tests/furnace.dae")
-        ;
-
-    auto mesh = scene.meshes[0];
-
-    //-------------------------------------------------------------------------
-    // Textures are uploaded by loaders: these may have different sampling
-    // parameters.
-    //-------------------------------------------------------------------------
-
-    auto cm_loader = engine.gpu.Texture.Loader.Compressed;
-    auto nm_loader = engine.gpu.Texture.Loader.Default;
-
-    auto colormap =
-        cm_loader(vec4(0.5, 0.5, 0.5, 1))
-        //cm_loader("engine/stock/unsorted/tiles/AlienCarving/ColorMap.png")
-        //cm_loader("engine/stock/unsorted/tiles/BrickWall1/ColorMap.png")
-        //cm_loader("data/Girl/Girl_cm.png")
-        ;
-    colormap.info();
-
-    auto normalmap =
-        nm_loader(vec4(0.5, 0.5, 1, 0))
-        //nm_loader("engine/stock/unsorted/tiles/AlienCarving/NormalMap.png")
-        //nm_loader("engine/stock/unsorted/tiles/Concrete/Crusty/NormalMap.png")
-    ;
-
-    //-------------------------------------------------------------------------
     // Make it GPU VBOs (Vertex Buffer Object) and IBO (vertex indexing array)
     // Here we could use some kind of magic: we can extract shader uniforms
     // and vertex attributes, and then bind them to VBOs with same names.
@@ -128,17 +141,6 @@ void main()
     vao.unbind();
     ibo.unbind();
 
-    //-------------------------------------------------------------------------
-
-    auto mProjection = mat4.perspective(
-        game.screen.width, game.screen.height,
-        60,
-        1, 200
-    );
-
-    auto mView = mat4.look_at(vec3(0, -10, 2), vec3(0, 0, 2), vec3(0, 0, 1));
-    auto pLight = vec3(5, 3, 7);
-    
     //-------------------------------------------------------------------------
 
     void draw()
