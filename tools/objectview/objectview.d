def80fbcea299c6c13c909bb98d7ad6634e07e4c
Markus Koskimies <mkoskim@gmail.com>
2018-09-06 21:02:43 +0300

    Cleanups

--- tools/objectview/objectview.d
+++ tools/objectview/objectview.d
@@ -14,12 +14,6 @@ import std.stdio;
 
 import engine.gpu.util;
 
-//*****************************************************************************
-//
-static if(1)
-//
-//*****************************************************************************
-
 void main()
 {
     game.init(800, 600);
@@ -192,120 +186,6 @@ void main()
         &draw,  // Drawing
         null,   // list of actors
         null    // Event processing
-    );
-}
-
-//*****************************************************************************
-//
-else
-//
-//*****************************************************************************
-
-void main()
-{
-    //-------------------------------------------------------------------------
-    // Init game with default window size
-    //-------------------------------------------------------------------------
-
-    game.init();
-
-    auto pipeline = new scene3d.SimplePipeline();
-
-    with(pipeline)
-    {
-        cam = scene3d.Camera.basic3D(
-            0.1, 10,        // Near - far
-            scene3d.Grip.movable(0, 0, 5)
-        );
-
-        light = new scene3d.Light(
-            scene3d.Grip.fixed(2, 2, 0),    // Position
-            vec3(1, 1, 1),                  // Color
-            7.5,                            // Range
-            0.25                            // Ambient level
-        );
-    }
-
-    auto scene = engine.asset.SceneGraph.load("data/test.dae");
-    auto node = scene.lookup["Cube"];
-
-/*
-    auto node = pipeline.add(
-        scene3d.Grip.movable,
-            //blob.wavefront.loadmesh("engine/stock/mesh/Cube/CubeWrap.obj")
-            //blob.wavefront.loadmesh("engine/stock/mesh/Suzanne/Suzanne.obj"),
-            //blob.wavefront.loadmesh("engine/stock/mesh/Chess/bishop.obj"),
-            //blob.wavefront.loadmesh("engine/stock/mesh/Chess/king.obj"),
-            //blob.wavefront.loadmesh("engine/stock/mesh/Chess/knight.obj"),
-            //blob.wavefront.loadmesh("engine/stock/mesh/Chess/pawn.obj"),
-            //blob.wavefront.loadmesh("engine/stock/mesh/Chess/queen.obj"),
-            blob.wavefront.loadmesh("engine/stock/unsorted/mesh/Chess/rook.obj"),
-        pipeline.material(
-            //"engine/stock/tiles/Concrete/Dirty/ColorMap.png",
-            vec4(1, 0.8, 0, 1)
-            //"engine/stock/tiles/Concrete/Dirty/NormalMap.png",
-            //0.75
-        )
-    );
-*/
-
-    //-------------------------------------------------------------------------
-    // Control
-    //-------------------------------------------------------------------------
-
-    pipeline.actors.addcallback(() {
-        const float moverate = 0.25;
-        const float turnrate = 5;
-
-        node.grip.pos += vec3(
-            game.controller.axes[game.JOY.AXIS.LX],
-            0,
-            -game.controller.axes[game.JOY.AXIS.LY]
-        ) * moverate;
-
-        node.grip.rot += vec3(
-            game.controller.axes[game.JOY.AXIS.RY],
-            game.controller.axes[game.JOY.AXIS.RX],
-            0
-        ) * turnrate;
-    });
-
-    //-------------------------------------------------------------------------
-
-    pipeline.actors.reportperf();
-
-    //-------------------------------------------------------------------------
-
-    bool processevents(SDL_Event event)
-    {
-        switch(event.type)
-        {
-            default: break;
-            case SDL_KEYDOWN: switch(event.key.keysym.sym)
-            {
-                default: break;
-                //case SDLK_w: scene.shader.fill = !scene.shader.fill; break;
-                //case SDLK_e: scene.shader.enabled = !scene.shader.enabled; break;
-                /*
-                case SDLK_r: {
-                    static bool normmaps = true;
-                    normmaps = !normmaps;
-                    maze.shader.activate();
-                    maze.shader.uniform("useNormalMapping", normmaps);
-                } break;
-                */
-            }
-        }
-        return true;
-    }
-
-    //-------------------------------------------------------------------------
-
-    simple.gameloop(
-        50,             // FPS (limit)
-        &pipeline.draw,    // Drawing
-        pipeline.actors,   // list of actors
-        &processevents
     );
 }
 
