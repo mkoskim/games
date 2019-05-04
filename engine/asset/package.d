cd94bd7104593cc04d15f6a9fcfe50731f08e2ce
Markus Koskimies <mkoskim@gmail.com>
2019-05-04 06:45:37 +0300

    Asset loading improvements.
    Not perfect yet, but now there is no need for temporary buffers, but you can create VBO buffers straight from ASSIMP buffers. Worked also with file info, trying to get models loaded correctly if they are oriented differently in the resource file.

--- engine/asset/package.d
+++ engine/asset/package.d
@@ -1,12 +1,10 @@
 module engine.asset;
 
 public {
-    import engine.asset.scenegraph: SceneGraph, Option;
-    import engine.asset.scenegraph: load, loadmesh, loadmaterial;
+    import engine.asset.scenegraph;
     import engine.asset.material: Material;
     import engine.asset.bitmap: Bitmap;
     import engine.asset.font: Font;
     import engine.asset.lua: Lua;
-    //import geom = engine.asset.geom;
 }
 
