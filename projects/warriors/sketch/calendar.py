3d37e641a4334173f938b5e30e697504c98b5f90
Markus Koskimies <mkoskim@gmail.com>
2019-10-22 14:10:47 +0300

    Sketching

---
+++ projects/warriors/sketch/calendar.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+# "Calendar"
+#
+###############################################################################
+
+import sys
+import datetime
+
+def deg2time(deg):
+    #deg = (deg + 2*15) % 360
+    #print(deg * 24 / 360.0)
+    return datetime.time(deg * 24 / 360, (deg % 15) * 60 / 15)
+    
+def info(deg, year = 2019):
+    yday = (deg * 365 / 360)
+    date1 = datetime.date(year, 12, 24) + datetime.timedelta(yday)
+    date2 = datetime.date(year,  1,  1) + datetime.timedelta(yday)
+    hour = deg2time(deg)
+    
+    print("[%02d:%02d] %02d / %02d - %02d/%02d (%3d degs)" % (
+        hour.hour, hour.minute,
+        date1.day, date1.month,
+        date2.day, date2.month,
+        deg,
+    ))
+
+info( 0)
+info(15)
+info(30)
+info(45)
+info(60)
+info(75)
+
+info( 90 +  0)
+info( 90 + 15)
+info( 90 + 30)
+info( 90 + 45)
+info( 90 + 60)
+info( 90 + 75)
+
+info(180 +  0)
+info(180 + 15)
+info(180 + 30)
+info(180 + 45)
+info(180 + 60)
+info(180 + 75)
+
+info(270 +  0)
+info(270 + 15)
+info(270 + 30)
+info(270 + 45)
+info(270 + 60)
+info(270 + 75)
+
